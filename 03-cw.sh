f319c0ad0922d1d0a501fc26e8cf73efac6885c8
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-02 23:38:36 +0500

    Call MaxMax

--- 03-cw.sh
+++ 03-cw.sh
@@ -12,7 +12,5 @@ java -Xms16G -Xmx16G -cp $PWD/../chinese-whispers/target/chinese-whispers.jar \
      -N 200 -n 200 -e 0 \
      -in '02-edges.txt' -out '03-cw-wsi.txt'
 
-java -Xms16G -Xmx16G -cp $PWD/../chinese-whispers/target/chinese-whispers.jar \
-     de.tudarmstadt.lt.wsi.WSI -clustering cw \
-     -N 200 -n 200 -e 0 \
-     -in '02-edges-sim.txt' -out '03-cw-wsi-sim.txt'
+java -Xms16G -Xmx16G -jar $PWD/../maxmax/target/maxmax.jar \
+     -in '02-edges.txt' -out '03-maxmax.txt'
