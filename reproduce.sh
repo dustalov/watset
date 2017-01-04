daa91db0ca346acf8847a1915ec1d34569b76542
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 16:07:41 +0500

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -11,9 +11,21 @@ set -x
 
 make clean
 make data
+
+cp -fv data/edges.txt data/edges.count.txt
 make impl
+eval/pairwise.py --gold=data/ruthes-pairs.txt impl/*-pairs.txt | tee pairwise-count.tsv | column -t
+eval/pairwise.py --gold=data/yarn-pairs.txt impl/*-pairs.txt | tee pairwise-count.tsv | column -t
 
 make -C impl clean
-./similarities.py ../projlearn/all.norm-sz500-w10-cb0-it3-min5.w2v <data/edges.txt >data/edges.w2v.txt
-mv -fv data/edges.w2v.txt data/edges.txt
+sed -re 's/[[:digit:]]+$/1/g' -i data/edges.txt
 make impl
+eval/pairwise.py --gold=data/ruthes-pairs.txt impl/*-pairs.txt | tee pairwise-ones.tsv | column -t
+eval/pairwise.py --gold=data/yarn-pairs.txt impl/*-pairs.txt | tee pairwise-ones.tsv | column -t
+
+make -C impl clean
+./similarities.py ../projlearn/all.norm-sz500-w10-cb0-it3-min5.w2v <data/edges.count.txt >data/edges.w2v.txt
+cp -fv data/edges.w2v.txt data/edges.txt
+make impl
+eval/pairwise.py --gold=data/ruthes-pairs.txt impl/*-pairs.txt | tee pairwise-sim.tsv | column -t
+eval/pairwise.py --gold=data/yarn-pairs.txt impl/*-pairs.txt | tee pairwise-sim.tsv | column -t
