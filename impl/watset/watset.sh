1f15bf5d1759e2c88f2cd818269aadaf0ff78b25
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 12:41:18 +0500

    Simplify the disambiguation invocation

--- impl/watset/watset.sh
+++ impl/watset/watset.sh
@@ -7,21 +7,17 @@ java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers
      de.tudarmstadt.lt.wsi.WSI -clustering cw -cwOption TOP -N 200 -n 200 -e 0 \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../watset-wsi-cw-top.txt"
 
-$CWD/disambiguate.py "$CWD/../watset-wsi-cw-top.txt" >"$CWD/../watset-cw-top-senses.txt"
-
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.wsi.WSI -clustering cw -cwOption DIST_LOG -N 200 -n 200 -e 0 \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../watset-wsi-cw-log.txt"
-
-$CWD/disambiguate.py "$CWD/../watset-wsi-cw-log.txt" >"$CWD/../watset-cw-log-senses.txt"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.wsi.WSI -clustering cw -cwOption DIST_NOLOG -N 200 -n 200 -e 0 \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../watset-wsi-cw-nolog.txt"
 
-$CWD/disambiguate.py "$CWD/../watset-wsi-cw-nolog.txt" >"$CWD/../watset-cw-nolog-senses.txt"
-
 for setup in cw-top cw-log cw-nolog; do
+  $CWD/disambiguate.py "$CWD/../watset-wsi-$setup.txt" >"$CWD/../watset-$setup-senses.txt"
+
   java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
        de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption TOP \
        -in "$CWD/../watset-$setup-senses.txt" -out "$CWD/../watset-$setup-cw-top-synsets.txt"
