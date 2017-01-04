a72f9e26ab2ed044162874ee4c5ea806f9fc8db0
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 14:38:04 +0500

    Update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -9,13 +9,11 @@ EOF
 echo
 set -x
 
-./02-edges-batch.sh
-./03-cw.sh
-./05-edges-batch.sh
-./06-eval-pairs.awk 03-cw.txt >06-cw-pairs.txt
-./06-eval-pairs.awk 03-maxmax.txt >06-maxmax-pairs.txt
-./10-disambiguate.py >10-senses.txt
-./11-cw.sh
-./12-delabel.awk 11-synsets.txt >12-synsets.txt
-./13-lexicon.awk 12-synsets.txt >13-lexicon.txt
-./20-our-pairs.awk 12-synsets.txt >20-our-pairs.txt
+make clean
+make data
+make impl
+
+make -C impl clean
+./similarities.py ../projlearn/all.norm-sz500-w10-cb0-it3-min5.w2v <data/edges.txt >data/edges.w2v.txt
+mv -fv data/edges.w2v.txt data/edges.txt
+make impl
