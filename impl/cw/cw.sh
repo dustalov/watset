689fac6c75ca0004b698339648a942418f833bd5
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 12:33:35 +0500

    Add -synsets suffix to the synset files

--- impl/cw/cw.sh
+++ impl/cw/cw.sh
@@ -5,12 +5,12 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption TOP \
-     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-top.txt"
+     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-top-synsets.txt"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption DIST_LOG \
-     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-log.txt"
+     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-log-synsets.txt"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption DIST_NOLOG \
-     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-nolog.txt"
+     -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-nolog-synsets.txt"
