2a1efce74e84e44d7d3dc0f311b97571e1ac958e
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 14:41:05 +0500

    Make CW and MaxMax using TSV, too

--- impl/cw/cw.sh
+++ impl/cw/cw.sh
@@ -7,19 +7,19 @@ java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption TOP \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-top-clusters.txt"
 
-$CWD/../delabel.awk "$CWD/../cw-top-clusters.txt" > "$CWD/../cw-top-synsets.txt"
+$CWD/../delabel.awk "$CWD/../cw-top-clusters.txt" > "$CWD/../cw-top-synsets.tsv"
 rm -fv "$CWD/../cw-top-clusters.txt"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption DIST_LOG \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-log-clusters.txt"
 
-$CWD/../delabel.awk "$CWD/../cw-log-clusters.txt" > "$CWD/../cw-log-synsets.txt"
+$CWD/../delabel.awk "$CWD/../cw-log-clusters.txt" > "$CWD/../cw-log-synsets.tsv"
 rm -fv "$CWD/../cw-log-clusters.txt"
 
 java -Xms16G -Xmx16G -cp "$CWD/../../../chinese-whispers/target/chinese-whispers.jar" \
      de.tudarmstadt.lt.cw.global.CWGlobal -N 200 -cwOption DIST_NOLOG \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../cw-nolog-clusters.txt"
 
-$CWD/../delabel.awk "$CWD/../cw-nolog-clusters.txt" > "$CWD/../cw-nolog-synsets.txt"
+$CWD/../delabel.awk "$CWD/../cw-nolog-clusters.txt" > "$CWD/../cw-nolog-synsets.tsv"
 rm -fv "$CWD/../cw-nolog-clusters.txt"
--- impl/maxmax/maxmax.sh
+++ impl/maxmax/maxmax.sh
@@ -6,5 +6,5 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 java -Xms16G -Xmx16G -jar "$CWD/../../../maxmax/target/maxmax.jar" \
      -in "$CWD/../../data/edges.txt" -out "$CWD/../maxmax-clusters.txt"
 
-$CWD/../delabel.awk "$CWD/../maxmax-clusters.txt" > "$CWD/../maxmax-synsets.txt"
+$CWD/../delabel.awk "$CWD/../maxmax-clusters.txt" > "$CWD/../maxmax-synsets.tsv"
 rm -fv "$CWD/../maxmax-clusters.txt"
