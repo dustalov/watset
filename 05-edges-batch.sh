983d65e24ece1745445ddcbff6423c11ec7e9b64
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-04 14:57:01 +0500

    Remove yet another deprected script

--- 05-edges-batch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-export LANG=en_US.UTF-8
-./04-nodes.py
-LC_ALL=C sort --parallel=8 -s 04-edges-pre.txt |
-./01-count.awk > 05-edges.txt
-(echo 'source,target,weight,type'; sed -e 's/\t/,/g' -e 's/$/,undirected/g' <05-edges.txt) >05-edges.csv
