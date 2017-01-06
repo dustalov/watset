c4e5f07ad5b27d036237deed20a4aeab6d13802e
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-06 20:55:35 +0500

    Abandon csv in cnl.py

--- eval/cnl.py
+++ eval/cnl.py
@@ -4,7 +4,6 @@ from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
 
 import argparse
-import csv
 import sys
 
 parser = argparse.ArgumentParser()
@@ -14,9 +13,9 @@ args = vars(parser.parse_args())
 with open(args['lexicon']) as f:
     lexicon = {word.lower(): i for i, word in enumerate(f.read().splitlines())}
 
-reader = csv.reader(sys.stdin, delimiter='\t', quoting=csv.QUOTE_NONE)
+for row in sys.stdin:
+    _, _, words = row.rstrip().split('\t', 3)
 
-for id, _, words in reader:
     words = [word.lower() for word in words.split(', ')]
     words = [str(lexicon[word]) for word in words if word in lexicon]
 
