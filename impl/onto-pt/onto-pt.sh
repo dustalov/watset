adc1f1da24d64d6ee17731a8bd281ac6bbb0553b
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-07 20:50:03 +0500

    Precompute the edges

--- impl/onto-pt/onto-pt.sh
+++ impl/onto-pt/onto-pt.sh
@@ -7,10 +7,11 @@ DATA=$(mktemp -d)
 
 trap 'rm -rf -- "$DATA"' INT TERM HUP EXIT
 
+awk 'BEGIN { FS = OFS = "\t"; } $1 <= $2 { print $1, $2, $3; } $1 > $2 { print $2, $1, $3; }' "$CWD/../../data/edges.txt" |
+sort --parallel=$(nproc) -S1G -t $'\t' -su -k1,1 -k2,2 -o "$DATA/edges.txt"
+
 for i in $(seq 30); do
-  awk 'BEGIN { FS = OFS = "\t"; } $1 <= $2 { print $1, $2, $3; } $1 > $2 { print $2, $1, $3; }' "$CWD/../../data/edges.txt" |
-  sort --parallel=$(nproc) -S1G -t $'\t' -su -k1,1 -k2,2 |
-  $CWD/noise.awk > "$DATA/edges-$i.txt"
+  $CWD/noise.awk "$DATA/edges.txt" > "$DATA/edges-$i.txt"
 
   $CWD/../../../mcl-14-137/bin/mcl "$DATA/edges-$i.txt" \
     -te $(nproc) -I 1.6 --abc -o "$DATA/clusters-$i.txt" 2>/dev/null
