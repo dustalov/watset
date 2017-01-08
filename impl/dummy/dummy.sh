232f87a95a3a768935affe056e4b6c52edc88321
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-08 20:46:03 +0500

    Add a dummy method

---
+++ impl/dummy/dummy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+export LANG=en_US.UTF-8
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cut -f1 "$CWD/../../data/edges.txt" |
+sort --parallel=$(nproc) -su |
+awk 'BEGIN { FS = OFS = "\t"; } { print NR, 1, $1; }' > $CWD/../dummy-synsets.tsv
+cut -f1,2 "$CWD/../../data/edges.txt" > $CWD/../dummy-pairs.txt
