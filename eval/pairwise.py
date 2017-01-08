663511184c4d44dc3c6669bc8eb8775c172ba76d
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-08 20:56:29 +0500

    Remove the joint mode and rename conjoint to joint

--- eval/pairwise.py
+++ eval/pairwise.py
@@ -8,7 +8,7 @@ from multiprocessing import Pool, cpu_count
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gold', required=True)
-parser.add_argument('--lexicon', choices=('gold', 'joint', 'conjoint'), default='conjoint')
+parser.add_argument('--lexicon', choices=('gold', 'joint'), default='joint')
 parser.add_argument('--sorted', nargs='?', choices=('accuracy', 'precision', 'recall', 'f1'))
 parser.add_argument('path', nargs='*')
 args = vars(parser.parse_args())
@@ -55,8 +55,6 @@ with Pool(cpu_count()) as pool:
         resources[path] = (pairs, resource_lexicon)
 
 if args['lexicon'] == 'joint':
-    lexicon &= set.intersection(*(resource_lexicon for _, resource_lexicon in resources.values()))
-elif args['lexicon'] == 'conjoint':
     lexicon &= set.union(*(resource_lexicon for _, resource_lexicon in resources.values()))
 
 with Pool(cpu_count()) as pool:
