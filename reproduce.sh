219bc0d7d36e2b70f28c7dbc0fe2eb9b112d7b6e
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-14 17:06:12 +0500

    Remove the duplicated results and improve the preview

--- reproduce.sh
+++ reproduce.sh
@@ -22,6 +22,7 @@ make -C impl clean
 sed -re 's/[[:digit:]]+$/1/g' data/edges.count.txt > data/edges.ones.txt
 ln -sfTv edges.ones.txt data/edges.txt
 make impl
+rm -fv impl/{cpm,dummy}*.{txt,tsv}
 mkdir -p eval/ones
 mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ones
 
@@ -29,11 +30,12 @@ make -C impl clean
 ./similarities.py ../projlearn/all.norm-sz500-w10-cb0-it3-min5.w2v <data/edges.count.txt >data/edges.w2v.txt
 ln -sfTv edges.w2v.txt data/edges.txt
 make impl
+rm -fv impl/{cpm,dummy}*.{txt,tsv}
 mkdir -p eval/w2v
 mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/w2v
 
-eval/pairwise.py --gold=data/ruthes-pairs.txt data/yarn-pairs.txt eval/**/*-pairs.txt | tee pairwise-ruthes.tsv | column -t
-eval/pairwise.py --gold=data/yarn-pairs.txt data/ruthes-pairs.txt eval/**/*-pairs.txt | tee pairwise-yarn.tsv | column -t
+eval/pairwise.py --gold=data/ruthes-pairs.txt data/yarn-pairs.txt eval/**/*-pairs.txt | tee pairwise-ruthes.tsv | sort -t $'\t' -g -k6r | column -t
+eval/pairwise.py --gold=data/yarn-pairs.txt data/ruthes-pairs.txt eval/**/*-pairs.txt | tee pairwise-yarn.tsv | sort -t $'\t' -g -k6r | column -t
 
 eval/cluster.sh data/ruthes-synsets.tsv data/yarn-synsets.tsv eval/**/*-synsets.tsv | tee cluster-ruthes.tsv | column -t
 eval/cluster.sh data/yarn-synsets.tsv data/ruthes-synsets.tsv eval/**/*-synsets.tsv | tee cluster-yarn.tsv | column -t
