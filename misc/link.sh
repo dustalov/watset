9e2dac923d82ff189b745be234b263d2eeb0acaa
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-24 02:52:50 +0500

    Improve naming in link.sh

--- misc/link.sh
+++ misc/link.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 export LANG=en_US.UTF-8 LC_COLLATE=C
 
-./link.py --synsets=synsets.tsv --isas=pairs.txt | sort -t $'\t' -k2nr -k4nr -k1n -o joint.new.tsv
+./link.py --synsets=synsets.tsv --isas=pairs.txt | sort -t $'\t' -k2nr -k4nr -k1n -o linked.tsv
 
 sort --parallel=$(nproc) -S1G -t $'\t' -k1,1 -k2,2 -uo pairs-expanded.txt <pairs.txt <(./expanded.awk pairs-expansion.txt)
-./link.py --synsets=synsets.tsv --isas=pairs-expanded.txt | sort -t $'\t' -k2nr -k4nr -k1n -o joint-expanded.new.tsv
+./link.py --synsets=synsets.tsv --isas=pairs-expanded.txt | sort -t $'\t' -k2nr -k4nr -k1n -o linked-expanded.tsv
