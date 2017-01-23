dd9d93b84678baba755ad8301f9a3b5055c6c73b
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-24 00:14:13 +0500

    Revert "LC_CTYPE=C"
    This reverts commit c809541a5c7b018b0566f7726781200d1171afdb.

--- data/ru/edges.sh
+++ data/ru/edges.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-export LANG=en_US.UTF-8 LC_COLLATE=C LC_CTYPE=C
+export LANG=en_US.UTF-8 LC_COLLATE=C
 cat <(./wiktionary-edges.awk wiktionary.tsv) <(./abramov-edges.awk abramov.dat) <(./unldc-edges.awk unldc.tsv) |
 sed -e 's/ {2,}//g' |
 sort --parallel=$(nproc) -t $'\t' -S1G -k1 -k2 -s |
