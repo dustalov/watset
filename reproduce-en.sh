8f22a250d88361fc23c4a0bcd76619e78db67021
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-24 01:12:35 +0500

    Add {ru,en}/ prefixes

--- reproduce-en.sh
+++ reproduce-en.sh
@@ -15,30 +15,30 @@ make data-en
 mv -fv data/edges.txt data/edges.count.txt
 ln -sfTv edges.count.txt data/edges.txt
 make impl
-mkdir -p eval/count
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/count
+mkdir -p eval/en/count
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/en/count
 
 make -C impl clean
 sed -re 's/[[:digit:]]+$/1/g' data/edges.count.txt > data/edges.ones.txt
 ln -sfTv edges.ones.txt data/edges.txt
 make impl
 rm -fv impl/{cpm,dummy}*.{txt,tsv}
-mkdir -p eval/ones
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ones
+mkdir -p eval/en/ones
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/en/ones
 
 make -C impl clean
 ./similarities.py ../projlearn/GoogleNews-vectors-negative300.bin <data/edges.count.txt >data/edges.w2v.txt
 ln -sfTv edges.w2v.txt data/edges.txt
 make impl
 rm -fv impl/{cpm,dummy}*.{txt,tsv}
-mkdir -p eval/w2v
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/w2v
+mkdir -p eval/en/w2v
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/en/w2v
 
-eval/pairwise.py --gold=data/ru/wordnet-pairs.txt data/ru/twsi-pairs.txt eval/**/*-pairs.txt | tee pairwise-en-wordnet.tsv | sort -t $'\t' -g -k6r | column -t
-eval/pairwise.py --gold=data/ru/twsi-pairs.txt data/ru/wordnet-pairs.txt eval/**/*-pairs.txt | tee pairwise-en-twsi.tsv | sort -t $'\t' -g -k6r | column -t
+eval/en/pairwise.py --gold=data/en/wordnet-pairs.txt data/en/twsi-pairs.txt eval/en/**/*-pairs.txt | tee pairwise-en-wordnet.tsv | sort -t $'\t' -g -k6r | column -t
+eval/en/pairwise.py --gold=data/en/twsi-pairs.txt data/en/wordnet-pairs.txt eval/en/**/*-pairs.txt | tee pairwise-en-twsi.tsv | sort -t $'\t' -g -k6r | column -t
 
-eval/cluster.sh data/ru/wordnet-synsets.tsv data/ru/twsi-synsets.tsv eval/**/*-synsets.tsv | tee cluster-en-wordnet.tsv | column -t
-eval/cluster.sh data/ru/twsi-synsets.tsv data/ru/wordnet-synsets.tsv eval/**/*-synsets.tsv | tee cluster-en-twsi.tsv | column -t
+eval/en/cluster.sh data/en/wordnet-synsets.tsv data/en/twsi-synsets.tsv eval/en/**/*-synsets.tsv | tee cluster-en-wordnet.tsv | column -t
+eval/en/cluster.sh data/en/twsi-synsets.tsv data/en/wordnet-synsets.tsv eval/en/**/*-synsets.tsv | tee cluster-en-twsi.tsv | column -t
 
 join --header -j 1 -t $'\t' >results-en-wordnet.tsv \
   <(sed -re 's/-pairs.txt\t/\t/g' pairwise-en-wordnet.tsv) \
--- reproduce-ru.sh
+++ reproduce-ru.sh
@@ -15,30 +15,30 @@ make data-ru
 mv -fv data/edges.txt data/edges.count.txt
 ln -sfTv edges.count.txt data/edges.txt
 make impl
-mkdir -p eval/count
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/count
+mkdir -p eval/ru/count
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ru/count
 
 make -C impl clean
 sed -re 's/[[:digit:]]+$/1/g' data/edges.count.txt > data/edges.ones.txt
 ln -sfTv edges.ones.txt data/edges.txt
 make impl
 rm -fv impl/{cpm,dummy}*.{txt,tsv}
-mkdir -p eval/ones
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ones
+mkdir -p eval/ru/ones
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ru/ones
 
 make -C impl clean
 ./similarities.py ../projlearn/all.norm-sz500-w10-cb0-it3-min5.w2v <data/edges.count.txt >data/edges.w2v.txt
 ln -sfTv edges.w2v.txt data/edges.txt
 make impl
 rm -fv impl/{cpm,dummy}*.{txt,tsv}
-mkdir -p eval/w2v
-mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/w2v
+mkdir -p eval/ru/w2v
+mv -fv impl/*-pairs.txt impl/*-synsets.tsv eval/ru/w2v
 
-eval/pairwise.py --gold=data/ru/ruthes-pairs.txt data/ru/yarn-pairs.txt eval/**/*-pairs.txt | tee pairwise-ru-ruthes.tsv | sort -t $'\t' -g -k6r | column -t
-eval/pairwise.py --gold=data/ru/yarn-pairs.txt data/ru/ruthes-pairs.txt eval/**/*-pairs.txt | tee pairwise-ru-yarn.tsv | sort -t $'\t' -g -k6r | column -t
+eval/ru/pairwise.py --gold=data/ru/ruthes-pairs.txt data/ru/yarn-pairs.txt eval/ru/**/*-pairs.txt | tee pairwise-ru-ruthes.tsv | sort -t $'\t' -g -k6r | column -t
+eval/ru/pairwise.py --gold=data/ru/yarn-pairs.txt data/ru/ruthes-pairs.txt eval/ru/**/*-pairs.txt | tee pairwise-ru-yarn.tsv | sort -t $'\t' -g -k6r | column -t
 
-eval/cluster.sh data/ru/ruthes-synsets.tsv data/ru/yarn-synsets.tsv eval/**/*-synsets.tsv | tee cluster-ru-ruthes.tsv | column -t
-eval/cluster.sh data/ru/yarn-synsets.tsv data/ru/ruthes-synsets.tsv eval/**/*-synsets.tsv | tee cluster-ru-yarn.tsv | column -t
+eval/ru/cluster.sh data/ru/ruthes-synsets.tsv data/ru/yarn-synsets.tsv eval/ru/**/*-synsets.tsv | tee cluster-ru-ruthes.tsv | column -t
+eval/ru/cluster.sh data/ru/yarn-synsets.tsv data/ru/ruthes-synsets.tsv eval/ru/**/*-synsets.tsv | tee cluster-ru-yarn.tsv | column -t
 
 join --header -j 1 -t $'\t' >results-ru-ruthes.tsv \
   <(sed -re 's/-pairs.txt\t/\t/g' pairwise-ru-ruthes.tsv) \
