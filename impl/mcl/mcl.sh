4974076e09f9bbdf04f77c4708b54e0f55e3326b
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-27 04:24:41 +0500

    More MCL

--- impl/mcl/mcl.sh
+++ impl/mcl/mcl.sh
@@ -4,7 +4,7 @@ export LANG=en_US.UTF-8 LC_COLLATE=C
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 $CWD/../../../mcl-14-137/bin/mcl "$CWD/../../data/edges.txt" \
-  -te $(nproc) -I 1.6 --abc -o "$CWD/../mcl-clusters.txt" 2>/dev/null
+  -te $(nproc) --abc -o "$CWD/../mcl-clusters.txt" 2>/dev/null
 
 $CWD/format.awk "$CWD/../mcl-clusters.txt" | $CWD/../delabel.awk > "$CWD/../mcl-synsets.tsv"
 $CWD/../../pairs.awk "$CWD/../mcl-synsets.tsv" > "$CWD/../mcl-pairs.txt"
