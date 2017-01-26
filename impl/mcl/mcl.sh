9ebe2b4421c99878f59df0f487df1e06224dd05a
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-26 13:25:37 +0500

    Add the vanilla MCL

---
+++ impl/mcl/mcl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+export LANG=en_US.UTF-8 LC_COLLATE=C
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+$CWD/../../../mcl-14-137/bin/mcl "$CWD/../../data/edges.txt" \
+  -te $(nproc) -I 1.6 --abc -o "$CWD/../mcl-clusters.txt" 2>/dev/null
+
+$CWD/format.awk "$CWD/../mcl-clusters.txt" | $CWD/../delabel.awk > "$CWD/../mcl-synsets.tsv"
+$CWD/../../pairs.awk "$CWD/../mcl-synsets.tsv" > "$CWD/../mcl-pairs.txt"
+rm -fv "$CWD/../mcl-clusters.txt"
