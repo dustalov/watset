d9db9ebb544899b756bcd134aec7ec75feaa3082
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-01-26 13:12:19 +0500

    Compress the MCL output for ECO

--- impl/eco/eco.sh
+++ impl/eco/eco.sh
@@ -10,15 +10,19 @@ trap 'rm -rf -- "$DATA"' INT TERM HUP EXIT
 awk 'BEGIN { FS = OFS = "\t"; } $1 <= $2 { print $1, $2, $3; } $1 > $2 { print $2, $1, $3; }' "$CWD/../../data/edges.txt" |
 sort --parallel=$(nproc) -S1G -t $'\t' -su -k1 -k2 -o "$DATA/edges.txt"
 
+rm -fv "$CWD/../eco-mcl.txt.xz"
+
 for i in $(seq 30); do
   $CWD/noise.awk "$DATA/edges.txt" > "$DATA/edges-$i.txt"
 
   $CWD/../../../mcl-14-137/bin/mcl "$DATA/edges-$i.txt" \
     -te $(nproc) -I 1.6 --abc -o "$DATA/clusters-$i.txt" 2>/dev/null
+
+  xz -T $(nproc) "$DATA/clusters-$i.txt" -c >>"$CWD/../eco-mcl.txt.xz"
+
+  rm -fv "$DATA/clusters-$i.txt"
 done
 
-find "$DATA" -name 'clusters-*.txt' -exec cat {} \; > "$CWD/../eco-mcl.txt"
-
-$CWD/discover.py < "$CWD/../eco-mcl.txt" | $CWD/../delabel.awk > "$CWD/../eco-synsets.tsv"
+xzcat "$CWD/../eco-mcl.txt.xz" | $CWD/discover.py | $CWD/../delabel.awk > "$CWD/../eco-synsets.tsv"
 
 $CWD/../../pairs.awk "$CWD/../eco-synsets.tsv" > "$CWD/../eco-pairs.txt"
