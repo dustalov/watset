9686d953484480892693b60680580ba318d73f6a
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-04 01:54:16 +0500

    Improve make clean and add expand.sh

---
+++ misc/expand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -ex
+export LANG=en_US.UTF-8 LC_COLLATE=C
+
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+W2V=all.norm-sz500-w10-cb0-it3-min5.w2v
+
+CLUSTERS=sz500-k20-l1.0
+
+MODEL=regularized_synonym
+
+for ISAS in $@; do
+  EXPANDED=${ISAS%-isas.txt}-exp-isas.txt
+
+  $CWD/../../projlearn/expand.py \
+    --w2v=$CWD/../../projlearn/$W2V \
+    --kmeans=$CWD/../../projlearn/$CLUSTERS/kmeans.pickle \
+    --path=$CWD/../../projlearn/$CLUSTERS \
+    --model=$MODEL $ISAS |
+  $CWD/expanded.awk >$EXPANDED
+done
