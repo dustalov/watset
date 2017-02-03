4cc0101909d4c8577971a2cf4451aaaefdf1445d
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-04 03:52:38 +0500

    Fix the output filename

--- misc/reproduce.sh
+++ misc/reproduce.sh
@@ -3,20 +3,18 @@ export LANG=en_US.UTF-8 LC_COLLATE=C
 
 for WEIGHT in tf idf tfidf; do
 for SYNSETS in *-synsets.tsv; do
-for PAIRS in patterns-isas.txt; do
 for ISAS in {patterns,wiktionary,mas}{,-exp}-isas.txt; do
   if [ ! -f "$ISAS" ]; then
     touch "$ISAS"
   fi
 
-  LINKED=${SYNSETS%-synsets.tsv}-${ISAS%-isas.txt}-linked.tsv
+  LINKED=${SYNSETS%-synsets.tsv}-${ISAS%-isas.txt}-$WEIGHT-linked.tsv
   ./link.py --synsets=$SYNSETS --isas=$ISAS | sort -t $'\t' -k2nr -k4nr -k1n -o "$LINKED"
 
   ISAS=${LINKED%-linked.tsv}-isas.txt
   ./linked-isas.awk "$LINKED" > "$ISAS"
 
   EVALUATE="$EVALUATE $ISAS"
-done
 done
 done
 done
