b03e9778ad39064b7b792a5936a5e88697347612
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-04 05:17:56 +0500

    Now filter.py can ignore hypernyms

--- misc/filter.py
+++ misc/filter.py
@@ -9,11 +9,12 @@ import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument('lexicon')
+parser.add_argument('-1', action='store_true', dest='first')
 args = vars(parser.parse_args())
 
 with open(args['lexicon']) as f:
     lexicon = {word for word in f.read().splitlines()}
 
 for row in csv.reader(sys.stdin, delimiter='\t'):
-    if row[0] in lexicon and row[1] in lexicon:
+    if (row[0] in lexicon and row[1] in lexicon) or (args['first'] and row[0] in lexicon):
         print('\t'.join((row[0], row[1])))
