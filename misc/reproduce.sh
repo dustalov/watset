638a884b95dff66191c4e7fd96656615aadabf34
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-04 05:08:32 +0500

    Sort the results

--- misc/reproduce.sh
+++ misc/reproduce.sh
@@ -19,4 +19,4 @@ done
 done
 done
 
-./evaluate.py --gold=ruthes-isas.txt {patterns,wiktionary,mas}{,-exp}-isas.txt $EVALUATE | tee 'pairwise-ruthes.tsv' | column -t
+./evaluate.py --gold=ruthes-isas.txt {patterns,wiktionary,mas}{,-exp}-isas.txt $EVALUATE | tee pairwise-ruthes.tsv | sort -t $'\t' -g -k6r | column -t
