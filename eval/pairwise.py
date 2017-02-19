5301904d9a1e32ec874573d322cf8e851b4e50f1
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-20 04:34:58 +0500

    WSRT for pairwise.py and judgements.py

--- eval/pairwise.py
+++ eval/pairwise.py
@@ -2,13 +2,20 @@
 
 import argparse
 import csv
+import itertools
 from concurrent.futures import ProcessPoolExecutor
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from collections import OrderedDict
+from scipy.stats import wilcoxon
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gold', required=True)
+parser.add_argument('--significance', nargs='?', choices=('false', 'precision', 'recall', 'f1'), default='false')
+parser.add_argument('--alpha', nargs='?', type=float, default=0.01)
 parser.add_argument('path', nargs='+')
 args = parser.parse_args()
+
+metric_score = None if args.significance == 'false' else globals()[args.significance + '_score']
 
 def synonyms(path):
     pairs = set()
@@ -37,7 +44,19 @@ lexicon = words(gold) & set.union(*(words(pairs) for pairs in resources.values()
 union = [pair for pair in gold | set.union(*resources.values()) if pair[0] in lexicon and pair[1] in lexicon]
 true  = [int(pair in gold) for pair in union]
 
+lexicon = sorted(lexicon)
+
+def wordwise(resource, word):
+    pairs = [pair for pair in union if pair[0] == word or pair[1] == word]
+
+    true  = [int(pair in resource) for pair in pairs]
+    pred  = [int(pair in gold)     for pair in pairs]
+
+    return (true, pred)
+
 def evaluate(path):
+    scores = OrderedDict((word, metric_score(*wordwise(resources[path], word))) for word in lexicon) if metric_score is not None else {}
+
     true = [int(pair in gold)            for pair in union]
     pred = [int(pair in resources[path]) for pair in union]
 
@@ -50,13 +69,26 @@ def evaluate(path):
         'tp':        tp,
         'precision': precision_score(true, pred),
         'recall':    recall_score(true, pred),
-        'f1':        f1_score(true, pred)
+        'f1':        f1_score(true, pred),
+        'scores':    scores
     }
 
 with ProcessPoolExecutor() as executor:
     results = {path: result for path, result in zip(resources.keys(), executor.map(evaluate, resources.keys()))}
 
-print('\t'.join(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1')))
+if metric_score is not None:
+    results = OrderedDict(item for item in sorted(results.items(), key=lambda x: x[1][args.significance], reverse=True))
+
+    def pairwise(iterable):
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    for (x_path, x_result), (_, y_result) in pairwise(results.items()):
+        x, y = list(x_result['scores'].values()), list(y_result['scores'].values())
+        results[x_path]['sign'] = int(wilcoxon(x, y).pvalue < args.alpha)
+
+print('\t'.join(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1', 'sign')))
 
 for path, values in results.items():
     print('\t'.join((
@@ -69,4 +101,5 @@ for path, values in results.items():
         str(values['precision']),
         str(values['recall']),
         str(values['f1']),
+        str(values.get('sign', 'e'))
     )))
--- misc/judgements.py
+++ misc/judgements.py
@@ -3,18 +3,24 @@
 import argparse
 import csv
 from collections import OrderedDict, defaultdict
-import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor
 import operator
 import sys
+import itertools
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from scipy.stats import wilcoxon
 
 from signal import signal, SIGINT
 signal(SIGINT, lambda signum, frame: sys.exit(1))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--sample', required=True, type=argparse.FileType('r'))
+parser.add_argument('--significance', nargs='?', choices=('false', 'precision', 'recall', 'f1'), default='false')
+parser.add_argument('--alpha', nargs='?', type=float, default=0.01)
 parser.add_argument('path', nargs='+', type=argparse.FileType('r'))
 args = parser.parse_args()
+
+metric_score = None if args.significance == 'false' else globals()[args.significance + '_score']
 
 gold = defaultdict(lambda: False)
 
@@ -31,9 +37,7 @@ for path in args.path:
 
                 gold[(hyponym, hypernym)] = (row['OUTPUT:judgement'].lower() == 'true')
 
-# import IPython; IPython.embed()
-
-results = defaultdict(list)
+resources = defaultdict(list)
 
 with args.sample as f:
     for row in csv.DictReader(f, delimiter='\t', quoting=csv.QUOTE_NONE):
@@ -41,13 +45,64 @@ with args.sample as f:
 
         assert hypernym is None or (hyponym, hypernym) in gold, (hyponym, hypernym)
 
-        results[row['path']].append((hyponym, hypernym))
+        resources[row['path']].append((hyponym, hypernym))
 
-writer = csv.writer(sys.stdout, delimiter='\t', lineterminator='\n')
-writer.writerow(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1'))
+lexicon = sorted({hyponym for pairs in resources.values() for hyponym, _ in pairs})
 
-for path, pairs in results.items():
-    true = [pair[1] is None or gold[pair] for pair in pairs]
-    pred = [pair[1] is not None           for pair in pairs]
+def wordwise(resource, word):
+    pairs = [pair for pair in resource if pair[0] == word]
 
-    writer.writerow((path, len(pairs), *confusion_matrix(true, pred).ravel(), precision_score(true, pred), recall_score(true, pred), f1_score(true, pred)))
+    true = [int(pair[1] is None or gold[pair]) for pair in pairs]
+    pred = [int(pair[1] is not None)           for pair in pairs]
+
+    return (true, pred)
+
+def evaluate(path):
+    scores = OrderedDict((word, metric_score(*wordwise(resources[path], word))) for word in lexicon) if metric_score is not None else {}
+
+    true = [int(pair[1] is None or gold[pair]) for pair in resources[path]]
+    pred = [int(pair[1] is not None)           for pair in resources[path]]
+
+    tn, fp, fn, tp = confusion_matrix(true, pred).ravel()
+
+    return {
+        'tn':        tn,
+        'fp':        fp,
+        'fn':        fn,
+        'tp':        tp,
+        'precision': precision_score(true, pred),
+        'recall':    recall_score(true, pred),
+        'f1':        f1_score(true, pred),
+        'scores':    scores
+    }
+
+with ProcessPoolExecutor() as executor:
+    results = {path: result for path, result in zip(resources.keys(), executor.map(evaluate, resources.keys()))}
+
+if metric_score is not None:
+    results = OrderedDict(item for item in sorted(results.items(), key=lambda x: x[1][args.significance], reverse=True))
+
+    def pairwise(iterable):
+        a, b = itertools.tee(iterable)
+        next(b, None)
+        return zip(a, b)
+
+    for (x_path, x_result), (_, y_result) in pairwise(results.items()):
+        x, y = list(x_result['scores'].values()), list(y_result['scores'].values())
+        results[x_path]['sign'] = int(wilcoxon(x, y).pvalue < args.alpha)
+
+print('\t'.join(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1', 'sign')))
+
+for path, values in results.items():
+    print('\t'.join((
+        path,
+        str(len(resources[path])),
+        str(values['tn']),
+        str(values['fp']),
+        str(values['fn']),
+        str(values['tp']),
+        str(values['precision']),
+        str(values['recall']),
+        str(values['f1']),
+        str(values.get('sign', int(False)))
+    )))
