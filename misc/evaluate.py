c8f8337829b2c9e9adaae466feb6f8d55be86996
Dmitry Ustalov <dmitry.ustalov@gmail.com>
2017-02-21 06:41:29 +0500

    Add --significance to evaluate.py

--- misc/evaluate.py
+++ misc/evaluate.py
@@ -2,14 +2,21 @@
 
 import argparse
 import csv
+import itertools
 from concurrent.futures import ProcessPoolExecutor
 import networkx as nx
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+from collections import defaultdict
+from scipy.stats import wilcoxon
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gold', required=True)
+parser.add_argument('--significance', action='store_true')
+parser.add_argument('--alpha', nargs='?', type=float, default=0.01)
 parser.add_argument('path', nargs='+')
 args = parser.parse_args()
+
+METRICS = {metric: globals()[metric + '_score'] for metric in ('precision', 'recall', 'f1')}
 
 def sanitize(str):
     return str.lower().replace(' ', '_')
@@ -37,11 +44,31 @@ lexicon = set(gold.nodes()) & set.union(*(set(G.nodes()) for G in resources.valu
 union = [pair for pair in set(gold.edges()) | set.union(*(set(G.edges()) for G in resources.values())) if pair[0] in lexicon and pair[1] in lexicon]
 true  = [int(nx.has_path(gold, *pair)) for pair in union]
 
-def tables(G):
-    pred = [int(pair[0] in G and pair[1] in G and nx.has_path(G, *pair)) for pair in union]
-    return (true, pred)
+index = defaultdict(list)
 
-def scores(true, pred):
+for pair in union:
+    for word in pair:
+        index[word].append(pair)
+
+def wordwise(G, word):
+    word_true = [int(nx.has_path(gold, *pair))                                for pair in index[word]]
+    word_pred = [int(pair[0] in G and pair[1] in G and nx.has_path(G, *pair)) for pair in index[word]]
+
+    return (word_true, word_pred)
+
+def scores(G):
+    if not args.significance:
+        return
+
+    labels = [wordwise(G, word) for word in lexicon]
+
+    return {metric: [score(*true_pred) for true_pred in labels] for metric, score in METRICS.items()}
+
+def evaluate(path):
+    G = resources[path]
+
+    pred = [int(pair[0] in G and pair[1] in G and nx.has_path(G, *pair)) for pair in union]
+
     tn, fp, fn, tp = confusion_matrix(true, pred).ravel()
 
     return {
@@ -51,16 +78,41 @@ def scores(true, pred):
         'tp':        tp,
         'precision': precision_score(true, pred),
         'recall':    recall_score(true, pred),
-        'f1':        f1_score(true, pred)
+        'f1':        f1_score(true, pred),
+        'scores':    scores(G)
     }
 
-def evaluate(path):
-    return scores(*tables(resources[path]))
+with ProcessPoolExecutor() as executor:
+    results = {path: result for path, result in zip(resources, executor.map(evaluate, resources))}
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def significance(metric):
+    if not args.significance:
+        return {}
+
+    desc, rank = sorted(results.items(), key=lambda item: item[1][metric], reverse=True), 1
+
+    ranks = {}
+
+    for (path1, results1), (path2, results2) in pairwise(desc):
+        x, y = list(results1['scores'][metric]), list(results2['scores'][metric])
+
+        ranks[path1] = rank
+
+        rank += int(wilcoxon(x, y).pvalue < args.alpha)
+
+        ranks[path2] = rank
+
+    return ranks
 
 with ProcessPoolExecutor() as executor:
-    results = {path: result for path, result in zip(resources.keys(), executor.map(evaluate, resources.keys()))}
+    ranks = {metric: result for metric, result in zip(METRICS, executor.map(significance, METRICS))}
 
-print('\t'.join(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1')))
+print('\t'.join(('path', 'pairs', 'tn', 'fp', 'fn', 'tp', 'precision', 'recall', 'f1', 'precision_rank', 'recall_rank', 'f1_rank')))
 
 for path, values in results.items():
     print('\t'.join((
@@ -72,5 +124,8 @@ for path, values in results.items():
         str(values['tp']),
         str(values['precision']),
         str(values['recall']),
-        str(values['f1'])
+        str(values['f1']),
+        str(ranks['precision'].get(path, 0)),
+        str(ranks['recall'   ].get(path, 0)),
+        str(ranks['f1'       ].get(path, 0))
     )))
